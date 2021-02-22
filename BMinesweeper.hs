e6b237cd739d79c9e41d43207966119ca029a450
bwoody13 <bwoody13@users.noreply.github.com>
2021-02-22 00:20:26 -0800

    Changed randLoc to create a list of n random locations

--- BMinesweeper.hs
+++ BMinesweeper.hs
@@ -6,8 +6,6 @@ import JMinesweeper
 import System.IO
 import System.Random
 
-type Location = (Int, Int)
-
 {-- each Cell is a cell with the contents of the cell
 data Cell = Cell Content State Location
               deriving (Show, Eq)
@@ -29,10 +27,21 @@ testBoard = makeBoard 5
 
 -- Bomb Generation --
 -- Place bombs on the board randomly
+-- TODO: should take the list of bombs in randLoc and look for those locations, and if it finds the location
+    -- it should change that cell to be a bomb
 -- placeBombs :: Board -> Board
 --PUT FN here
 
+-- gets a list of n location for bombs to be replaced (allows duplicates, that should be fixed)
+-- randLoc :: Int -> Int -> [Location]
+randLoc size n =
+    do
+        xg <- newStdGen
+        yg <- newStdGen    
+        return (zip (take n (randomRs (0, size) xg)) (take n (randomRs (0, size) yg)))
 
+{-
+OLD IMPLEMENTATION
 -- gets a list of n random locations for bombs to go // causing issues as No instance for (Show (IO (Integer, Integer))
 getBombLoc size 1 = [randLoc size]
 getBombLoc size n = (randLoc size) : (getBombLoc size (n-1)) 
@@ -44,6 +53,6 @@ randLoc size =
         x <- randomRIO (0,size) 
         y <- randomRIO (0,size)
         return (x,y) 
-
+-}
 
 
