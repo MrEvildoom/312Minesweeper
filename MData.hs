67d79bf509a4b6e9f167a139d3599793807790be
Jack Heidal <jpheidal@gmail.com>
2021-02-24 15:12:34 -0800

    Moved Neighbors to MData

--- MData.hs
+++ MData.hs
@@ -62,3 +62,12 @@ setState b loc state = map (setStateRow loc state) b
 -- gets a board's size so we don't have to always give size as an argument
 getSize :: Board -> Size
 getSize b = (length (b !! 0), length b)
+
+-- Takes in a location and returns a list of neighbors that are within bounds
+findNeighbors :: Location -> Size -> [Location]
+findNeighbors location size = filter (isvalid size) (makeNeighbors location)
+    where
+      isvalid :: Location -> Location -> Bool
+      isvalid (xbound, ybound) (x, y) = ((x >= 0) && (x < xbound)) && ((y >= 0) && (y < ybound))
+      makeNeighbors :: Location -> [Location]
+      makeNeighbors (x, y) = (x-1,y-1):(x,y-1):(x+1,y-1) : (x-1,y):(x+1,y) : (x-1,y+1):(x,y+1):(x+1,y+1):[]
