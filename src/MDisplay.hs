5c8f13088156750f2f2c90c035d4c09a3bfb0857
Jack Heidal <jpheidal@gmail.com>
2021-02-25 20:09:55 -0800

    minimum viable display

--- src/MDisplay.hs
+++ src/MDisplay.hs
@@ -6,12 +6,15 @@ module MDisplay where
 Displays, when printed, look like this
 
   00 Bombs
-  a b c d e
-1 X X F 1 0
-2 X X 2 0 0
-3 X F 1 1 1
+  0 1 2 3 4
+0 X X F 1 0
+1 X X 2 0 0
+2 X F 1 1 1
+3 X X X X X
 4 X X X X X
-5 X X X X X
+
+input "(x, y, c)"
+   or "(x, y, r)"
 
 X is a covered square
 numbers are clues
