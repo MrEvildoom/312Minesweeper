3dce1be719beacd43273fda7001b955554690b23
Jack Heidal <jpheidal@gmail.com>
2021-02-25 19:21:29 -0800

    Fixed tests

--- test/GenerationTests.hs
+++ test/GenerationTests.hs
@@ -7,8 +7,8 @@ testMBSize1 = TestCase (assertEqual "for: makeBoard (10, 8)"
                                      (10, 8)
                                      ((length mb108), (length (mb108 !! 0))))
 testMBSize2 = TestCase (assertEqual "for: makeBoard (18, 14)"
-                                    (18, 4)
-                                    ((length mb108), (length (mb108 !! 0))))
-testMBSize3 = TestCase (assertEqual "for: makeBoard (18, 14)"
-                                    (18, 4)
-                                    ((length mb108), (length (mb108 !! 0))))
+                                    (18, 14)
+                                    ((length mb108), (length (mb1814 !! 0))))
+testMBSize3 = TestCase (assertEqual "for: makeBoard (24, 20)"
+                                    (24, 20)
+                                    ((length mb108), (length (mb2420 !! 0))))
