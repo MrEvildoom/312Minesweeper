eebeb8b42d88ec58e11ca3d757ddaa0c6cbea549
Jack Heidal <jpheidal@gmail.com>
2021-02-26 23:21:24 -0800

    Some bugfixes

--- src/Interaction.hs
+++ src/Interaction.hs
@@ -18,12 +18,15 @@ clickGame game loc = game
 -- given a board and location, find the cell on the board to operate on.
 -- if the cell at loc is revealed then do nothing
 click :: Game -> Location -> Game
-click g loc = map (clickRow loc) (g Board)
+click (Gamestate size bombs board winState) loc =
+      (Gamestate size bombs (map (clickRow loc) board) winState)
   where clickRow ::Location -> Row -> Row
         clickRow loc row = map (\ (CellC cc cs cl) ->
             if cl == loc
-            then clickCell (CellC cc cs cl) g loc
+            then clickCell (CellC cc cs cl)
+                           (Gamestate size bombs board winState) loc
             else (CellC cc cs cl)) row
+        getBoard (Gamestate size bombs board winState) = board
 
 -- given a Cell,
 -- if it is already revealed, do nothing
@@ -32,37 +35,46 @@ click g loc = map (clickRow loc) (g Board)
 clickCell :: Cell -> Game -> Location -> Game
 clickCell (CellC cc cs cl) (Gamestate size bombs board winState) loc =
   if cs == Uncovered
-  then (Gamestate size bombs board winState) 	-- return a message saying that this location is already revealed
+  then (Gamestate size bombs board winState)
+  -- return a message saying that this location is already revealed
   else if cs == Flagged
-  then (Gamestate size bombs board winState) -- return a message saying that this location has been flagged, must be unflagged to uncover.
+  then (Gamestate size bombs board winState)
+  -- return a message saying that this location has been flagged, must be unflagged to uncover.
   else if cc == Bomb
-  then (Gamestate size bombs board Loss) 	-- TODO: game over. Return to original I/O console message. 
-  else       
-   checkWinCondition 
-    (Cell C cc cs cl)
-    (GameState size bombs (revealSpread board [loc] []) winState)-- uncover this cell and change the state of everything that should be revealed.
+  then (Gamestate size bombs board Loss)
+  -- TODO: game over. Return to original I/O console message.
+  else
+   checkWinCondition
+    (CellC cc cs cl)
+    (Gamestate size bombs (revealSpread board [loc] []) winState)
+    -- uncover this cell and change the state of everything that should be revealed.
 
 -- check win condition, if not met then reach just reveal the board spread.
 -- the win condition: # of non-bomb cells revealed + # of remaining uncovered bomb tiles = total tiles on the board.
 checkWinCondition :: Cell -> Game -> Game
-checkWinCondition (CellC cc cs cl) (GameState size bombs (l:ls) winState) =
+checkWinCondition (CellC cc cs cl) (Gamestate size bombs (l:ls) winState) =
   if ((countBombsFn (l:ls)) + (countRevealedCells (l:ls))) == (length l) * (length ls)
-  then (Gamestate size bombs (l:ls) Win) -- TODO: win condition, terminate the function
+  then (Gamestate size bombs (l:ls) Win)
+  -- TODO: win condition, terminate the function
   else (Gamestate size bombs (l:ls) winState)
 
 -- should count all the bombs on the board
-countBombsFn :: Board -> Location -> Num
-countBombs xss = sum [1 | xs <- xss, x <- xs, (getContent xss (x location)) == Bomb] 
+countBombsFn :: Board -> Location -> Int
+countBombsFn board loc = sum [1 | row <- board,
+                                  cell <- row,
+                                  (getContent board loc) == Bomb]
 
 -- should count all the currently revealed cells on the board
-countRevealedCells :: Board -> Location -> Num
-countRevealedCells xss = sum [1 | xs <- xss, x <- xs, (getState xss (x location)) == Uncovered] 
+countRevealedCells :: Board -> Location -> Int
+countRevealedCells board loc = sum [1 | row <- board,
+                                        cell <- row,
+                                        (getState board loc) == Uncovered]
 
 -- FLAGGING A CELL --
 --flags the locaiton and updates the game
 --TODO: make flagging a cell decrement the number of bombs and unflagging a cell increment
 flagGame:: Game -> Location -> Game
-flagGame (Gamestate size bombs board winstate) loc = 
+flagGame (Gamestate size bombs board winstate) loc =
   (Gamestate size bombs (flag board loc) winstate)
 
 -- given a board and location, flag the location, or unflag if it is already flagged
@@ -101,4 +113,3 @@ revealSpread b (l:ls) oldls
     -- cells are revealable if they are covered, not flagged, and a clue
     revealable :: Location -> Bool
     revealable loc = ((getContent b loc) /= Bomb) && ((getState b loc) == Covered)
-
